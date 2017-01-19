af354f3e0c298c12ade9e0eb82175ed4fa4c6000
Ben Tristem <github@bentristem.com>
2017-01-19 22:37:05 +0000

    Converted to Enemy.cs, and added hit points.

--- Assets/NPCs/Enemy.cs
+++ Assets/NPCs/Enemy.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OscillateChild : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
 
     [SerializeField] private float zMovementInMeters; // default to 0
+
     [SerializeField] [Range(0.5f, 60)]  // Note cannot be 0
     private float oscillationPeriodInSeconds = 4.0f;
+   
+    private float health = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // TODO fix this for runtime changes
+        // Optionally osciallates enemy position
         float offsetFraction = Mathf.Cos(Time.time * Mathf.PI / oscillationPeriodInSeconds);
         transform.localPosition += new Vector3(0, 0, zMovementInMeters * offsetFraction * Time.deltaTime);
+        //  TODO fix this for runtime changes
     }
 }
