40ffbc1d65931623db23a50f7d5948010f904dc2
Ben Tristem <github@bentristem.com>
2017-01-19 21:20:39 +0000

    Added Gamepad toggle button
    Crying out for generics, gonna do it cause I know how to party.

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,18 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+// Dependencies for gamepad toggling
+using UnityStandardAssets.Characters.ThirdPerson;
 
-    //[SerializeField] // To make visible in inspector
-    //private float weaponRangeInMeters = 10;
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void ToggleGamePad()
+    {
+        print("Toggling gamepad control.");
+
+        AICharacterControl aiCharControl = GetComponent<AICharacterControl>();
+        if (aiCharControl.isActiveAndEnabled)
+        {
+            aiCharControl.enabled = false;
+        }
+        else
+        {
+            aiCharControl.enabled = true;
+        }
+
+        CursorMovement cursorMovement = GetComponent<CursorMovement>();
+        if (cursorMovement.isActiveAndEnabled)
+        {
+            cursorMovement.enabled = false;
+        }
+        else
+        {
+            cursorMovement.enabled = true;
+        }
+
+        ThirdPersonUserControl thirdPersonUserControl = GetComponent<ThirdPersonUserControl>();
+        if (thirdPersonUserControl.isActiveAndEnabled)
+        {
+            thirdPersonUserControl.enabled = false;
+        }
+        else
+        {
+            thirdPersonUserControl.enabled = true;
+        }
+    }
 }
