51f5a6c66caa68e0ff35018f73e4a749ecab2719
Ben Tristem <github@bentristem.com>
2017-01-20 16:37:12 +0000

    Wrote generic ToggleComponenet<>() method

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,42 +5,27 @@ using System.Collections;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Player : MonoBehaviour {
+    
+    private void ToggleComponent<T>() where T : MonoBehaviour
+    {
+        T componentToToggle = GetComponent<T>();
+        if (componentToToggle.isActiveAndEnabled)
+        {
+            componentToToggle.enabled = false;
+        }
+        else
+        {
+            componentToToggle.enabled = true;
+        }
+    }
 
     // Toggles the necessary components to switch from gamepad to keyboard control
     // This method may be called directly from UI control such as a button or toggle
     public void ToggleGamePad()
     {
         print("Toggling gamepad control.");
-
-        // TODO discuss idea of creating a generic ToggleComponent method here. Use Gameobject or MonoBehavoiur?
-        AICharacterControl aiCharControl = GetComponent<AICharacterControl>();
-        if (aiCharControl.isActiveAndEnabled)
-        {
-            aiCharControl.enabled = false;
-        }
-        else
-        {
-            aiCharControl.enabled = true;
-        }
-
-        CursorMovement cursorMovement = GetComponent<CursorMovement>();
-        if (cursorMovement.isActiveAndEnabled)
-        {
-            cursorMovement.enabled = false;
-        }
-        else
-        {
-            cursorMovement.enabled = true;
-        }
-
-        ThirdPersonUserControl thirdPersonUserControl = GetComponent<ThirdPersonUserControl>();
-        if (thirdPersonUserControl.isActiveAndEnabled)
-        {
-            thirdPersonUserControl.enabled = false;
-        }
-        else
-        {
-            thirdPersonUserControl.enabled = true;
-        }
+        ToggleComponent<AICharacterControl>();
+        ToggleComponent<CursorMovement>();
+        ToggleComponent<ThirdPersonUserControl>();
     }
 }
