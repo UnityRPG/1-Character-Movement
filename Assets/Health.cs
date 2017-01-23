710b27864759e118afd8e63d16a958ce67ed5433
Ben Tristem <github@bentristem.com>
2017-01-23 23:12:17 +0000

    Added health indicators to enemies

---
+++ Assets/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	private float health = 1f;
+
+	TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponentInChildren<TextMesh> ();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		int healthPercent = (int)(health * 100); // Note bracketing
+		textMesh.text = healthPercent.ToString ();
+	}
+}
