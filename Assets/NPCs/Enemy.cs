8702a452319a534a7780b002942fc1f9d78e8fa8
Ben Tristem <github@bentristem.com>
2017-01-26 11:28:53 +0000

    Enemy damage radius gismos setup

---
+++ Assets/NPCs/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] [Tooltip("To visualise switch to Scene and select enemy(s)")]
+    float attackRadiusInM = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadiusInM);
+    }
+}
