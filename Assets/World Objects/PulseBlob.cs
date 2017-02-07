cb1b362423e83412dfde6519e719361ca994d027
Ben Tristem <github@bentristem.com>
2017-02-07 00:32:18 +0000

    Created blend-shape animation pass-through

---
+++ Assets/World Objects/PulseBlob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseBlob : MonoBehaviour {
+
+    Animator animator;
+    SkinnedMeshRenderer skinnedMeshRenderer;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        skinnedMeshRenderer.SetBlendShapeWeight(0, animator.GetFloat("Key 1"));
+        skinnedMeshRenderer.SetBlendShapeWeight(1, animator.GetFloat("Key 2"));
+    }
+}
