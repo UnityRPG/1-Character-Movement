647aec31f8ecd5da229575aae841cf3d3c737bc8
Ben Tristem <github@bentristem.com>
2017-02-09 18:53:04 +0000

    Floating damage points

---
+++ Assets/NPCs/DamageBubble.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageBubble : MonoBehaviour {
+
+    RectTransform rectTransform = null;
+    Text bubbleText = null;
+
+	// Use this for initialization
+	void Start () {
+        rectTransform = GetComponent<RectTransform>();
+        bubbleText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Note copy-paste from player
+    public void DoFixedDamage(int points) // TODO public required
+    {
+        bubbleText.text = points.ToString();
+        Invoke("FadeBubble", .5f);
+        // TODO trigger animation instead
+    }
+
+    void FadeBubble()
+    {
+        bubbleText.text = "";
+    }
+
+}
