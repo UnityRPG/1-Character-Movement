97b872f247e10a7aa5b8dd3e38ef830993ab2f0a
Ben Tristem <github@bentristem.com>
2017-02-09 17:11:42 +0000

    Health bar added to NPCs

--- Assets/NPCs/NPC_UI.cs
+++ Assets/NPCs/NPC_UI.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using RPG;
 
 public class NPC_UI : MonoBehaviour {
 
@@ -9,11 +11,21 @@ public class NPC_UI : MonoBehaviour {
     GameObject npcUIPrefab = null;
 
     Camera cameraToLookAt;
-    
+
+    [SerializeField]
+    RawImage healthBarRawImage;
+    Enemy enemy;
+
     // Use this for initialization 
     void Start()
     {
         cameraToLookAt = Camera.main;
+        enemy = GetComponentInParent<Enemy>();
+
+        print(enemy);
+        healthBarRawImage = GetComponentInChildren<RawImage>();
+        print(healthBarRawImage);
+
         Instantiate(npcUIPrefab, transform.position, Quaternion.identity, transform);
     }
 
@@ -22,6 +34,9 @@ public class NPC_UI : MonoBehaviour {
     {
         //var directionToLook = Quaternion.LookRotation(cameraToLookAt.transform.position - transform.position); 
         transform.LookAt(cameraToLookAt.transform);
-        transform.rotation = Quaternion.LookRotation(-cameraToLookAt.transform.forward);
+        transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+
+        float xValue = -(enemy.healthAsPercentage / 2f) - 0.5f;  // TODO note hard code
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
     }
 }
