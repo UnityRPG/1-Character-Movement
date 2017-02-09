9dbd79ac209178a3d63bfcfe3a529ee386ed80ec
Ben Tristem <github@bentristem.com>
2017-02-09 16:03:26 +0000

    NPC UI and Model separated
    To scale model use model child of NPC.

---
+++ Assets/AlignWithCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithCamera : MonoBehaviour {
+
+    Camera cameraToLookAt;
+
+    // Use this for initialization 
+    void Start()
+    {
+        cameraToLookAt = Camera.main;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        //var directionToLook = Quaternion.LookRotation(cameraToLookAt.transform.position - transform.position); 
+        transform.LookAt(cameraToLookAt.transform);
+        transform.rotation = Quaternion.LookRotation(-cameraToLookAt.transform.forward);
+    }
+}
