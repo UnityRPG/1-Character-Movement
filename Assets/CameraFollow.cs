d0e21f4cd55abab7e5db8b6c6712a75728e3a568
Ben Tristem <github@bentristem.com>
2017-02-17 22:51:13 +0000

    5_CM_RPG A Simple DIY Follow Camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position;
+	}
+}
